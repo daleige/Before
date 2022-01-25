60fc300ddd2ee7becfe1ec46d21da381d321bd51
陈扬齐 <766790183@qq.com>
2022-01-25 18:18:58 +0800

    feat:实现二维库功能

---
+++ service-qrcode/src/main/java/com/chenyangqi/service/qrcode/QrCodeTestActivity.kt
@@ -0,0 +1,12 @@
+package com.chenyangqi.service.qrcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chenyangqi.business.qrcode.R
+
+class QrCodeTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_test)
+    }
+}
